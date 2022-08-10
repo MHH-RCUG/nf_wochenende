32f3a1aa3ad38477a0ec4ae38656e9a527ab5bca
Colin Davenport <colindaven@hotmail.com>
2022-08-10 15:48:25 +0200

    cleanup code

--- start_nf.sh
+++ start_nf.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo "bash script to start the nf_wochenende pipeline"
+echo "Starting the nf_wochenende pipeline"
 echo "Requires nextflow in path. In case of problems, activate environment: conda activate wochenende"
 
-# get most current version - for developers
+#get most current version - for developers
 #git pull -q
 
 # ignore system JAVA_HOME, use that supplied by wochenende conda env for nextflow
@@ -13,6 +13,7 @@ unset HAYBALER_DIR
 # cleanup work/ 
 rm -rf work
 
+# Developer tests
 # get test files, create reference (small files). Do it yourself for bigger test references 
 # eg mock community from SRA https://github.com/colindaven/wochenende_manuscript/blob/main/mock/download_fastq.sh
 # eg mock ref file human22_zymo_test.fa
@@ -26,15 +27,3 @@ rm -rf work
 
 # Set reference and parameters in nextflow.config
 nextflow run nf_wochenende.nf  -with-timeline -with-report --fastq *R1.fastq 
-
-
-
-#########
-# Notes
-###########
-
-# run pipeline, no bash variables allowed as empty strings evaluate to true in groovy
-#nextflow run nf_wochenende.nf  -with-timeline -with-report -with-dag flowchart.dot --metagenome Ath --aligner minimap2long --remove_mismatching 250 --mq30 --readType SE --longread --no_dup_removal --no_abra --fastq test_sm.fastq
-
-# python3 /run_Wochenende.py --ref  --threads 16 --aligner minimap2-long   250 SE  --debug --force_restart test_sm.fastq
-
